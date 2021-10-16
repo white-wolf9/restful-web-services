eb5e1cbd8d496b52039764a6834fde543b14741d
Saurav Sanyal <sanyal.s271@gmail.com>
2021-10-16 18:32:25 +0530

    Versioning using Request Param
    -PersonController modified with 2 methods to handle versioning using params.

--- src/main/java/com/whitewolf9/rest/webservices/restfulwebservices/versioning/PersonController.java
+++ src/main/java/com/whitewolf9/rest/webservices/restfulwebservices/versioning/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
+    //  One of the ways to version API is to use two separate URIs
+
     @GetMapping("v1/person")
     public PersonV1 persionV1(){
         return new PersonV1("Bob Charlie");
@@ -13,6 +15,18 @@ public class PersonController {
 
     @GetMapping("v2/person")
     public PersonV2 personV2(){
+        return new PersonV2(new Name("Fred", "Allen"));
+    }
+
+    //  Second way of versioning API is as per below, using the request param.
+
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 personParamV1(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 personParamV2(){
         return new PersonV2(new Name("Fred", "Allen"));
     }
 
