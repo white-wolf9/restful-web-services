53710c833937f8708a1389658d80273a5c4be2ca
Saurav Sanyal <sanyal.s271@gmail.com>
2021-10-16 11:07:11 +0530

    Internationalization
    -HelloWorldController: Class modified to use a Message source and resource bundle to return the response message as per the locale specified in the request header.

--- src/main/java/com/whitewolf9/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/whitewolf9/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.whitewolf9.rest.webservices.restfulwebservices.controllers;
 
 import com.whitewolf9.rest.webservices.restfulwebservices.entity.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -22,6 +30,16 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+
+        //  Uses the ACCEPT-LANGUAGE from the request header to return the message
+        //  return messageSource.getMessage("good.morning.menssage",null, "Default Message", locale);
+
+        //  Uses the LocaleContextHolder to dynamically fetch the locale, if not specified returns EN
+        return messageSource.getMessage("good.morning.message",null, "Default Message", LocaleContextHolder.getLocale());
     }
 
 }
